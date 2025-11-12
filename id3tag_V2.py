972fdfc1443af5848ad5f2d110c97a956f547409
MitjaGo <65914869+MitjaGo@users.noreply.github.com>
2025-11-12 15:03:10 +0100

    Update id3tag_V2.py

--- id3tag_V2.py
+++ id3tag_V2.py
@@ -10,6 +10,24 @@ import zipfile
 import re
 from datetime import datetime
 
+#ERROR GENRE#
+#----------------#
+eyed3.log.setLevel("ERROR")  # Suppress warnings
+
+audiofile = eyed3.load(temp_path)
+if audiofile is None or audiofile.tag is None:
+    print("⚠️ Could not read tag data, possibly invalid genre.")
+else:
+    # Manually reset the genre if invalid
+    try:
+        if audiofile.tag.genre and audiofile.tag.genre.id == 255:
+            audiofile.tag.genre = None
+            audiofile.tag.save()
+    except Exception as e:
+        print("Skipping invalid genre:", e)
+
+#----------------#
+
 st.set_page_config(page_title="MP3 Tag Editor", page_icon="🎵", layout="centered")
 st.title("🎧 MP3 Metadata & Thumbnail Editor")
 
